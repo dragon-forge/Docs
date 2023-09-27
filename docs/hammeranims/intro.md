e4a2099ffd9cdf194bdb8941149e19872f8bb773
Zeith <me@zeith.org>
2023-09-27 18:37:35 +0300

    Initial commit.

---
+++ docs/hammeranims/intro.md
@@ -0,0 +1,82 @@
+---
+sidebar_position: 1
+---
+
+# Getting Started
+
+:::tip Getting familiar with geometry and animations
+Go grab yourself the [Blockbench](https://www.blockbench.net/) app and play around with it.
+
+This mod relies on Bedrock geometry and animation format, which is easy to do in Blockbench.
+
+The model type you are looking for is **Bedrock Entity**.
+:::
+
+Before using **Hammer Animations**, you should firstly add it to your workspace!
+
+## 1.19.2 and Above
+
+Firstly, add my repository to your workspace.
+The `repositories` block should already be present in your `build.gradle`
+
+```groovy
+repositories {
+    maven {
+        name = "Zeitheron Maven"
+        url = "https://maven.zeith.org/"
+        content {
+            includeGroupByRegex "org\\.zeith.*"
+        }
+    }
+}
+```
+
+After adding the repository, navigate to `dependencies` closure.
+In here you are going to add two dependencies:
+
+```groovy
+dependencies {
+    implementation fg.deobf("org.zeith.hammerlib:HammerLib-1.19.2:19.3.63")
+    implementation fg.deobf("org.zeith.hammeranims:HammerAnimations-1.19.2:19.2.14")
+}
+```
+
+**Make sure to replace the `1.19.2` with your game version, `19.3.63` with latest HammerLib version for given version, and `19.2.14` with the latest HammerAnimations version.**
+
+After this, refresh your project in IDE of your choice.
+
+You should see HammerLib and HammerAnimations appear in your classpath.
+
+## 1.12.2
+
+:::danger Warning!
+While 1.12.2 is very ancient version, HammerAnimations was built for 1.12.2 to support TerrariaCraft project.
+:::
+
+**Before proceeding**, make sure you are using `stable_39` MCP mappings. This is **very** important, and ignoring this is going to crash your game.
+
+After that is ensured, add my repository to your workspace.
+The `repositories` block should already be present in your `build.gradle`
+
+```groovy
+repositories {
+    maven {
+        name = "Zeitheron Maven"
+        url = "https://maven.zeith.org"
+    }
+}
+```
+
+After adding the repository, navigate to `dependencies` closure.
+In here you are going to add two dependencies:
+
+```groovy
+dependencies {
+    deobfCompile "org.zeith.HammerLib:HammerLib-1.12.2:2.0.6.37"
+    deobfCompile "org.zeith.hammeranims:HammerAnimations-1.12.2:12.2.14"
+}
+```
+
+After this, run `gradle setupDecompWorkspace idea` for IntelliJ IDEA or `gradle setupDecompWorkspace eclipse` for Eclipse IDE.
+
+You should see HammerLib and HammerAnimations appear in your classpath.
---
+++ docusaurus.config.js
@@ -0,0 +1,127 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Docs for Zeith\'s mods.',
+  tagline: 'Gettings started with HammerSeries framework (and other mod APIs)',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://docs.zeith.org/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Zeitheron', // Usually your GitHub org/user name.
+  projectName: 'Docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/banner.webp',
+      navbar: {
+        title: 'HammerAnimations',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.webp',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            href: 'https://github.com/dragon-forge',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://ds.zeith.org/dev',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/Zeitheron',
+              },
+              {
+                label: 'Bluesky',
+                href: 'https://bsky.app/profile/zeith.bsky.social',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/dragon-forge/HammerAnimations',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Zeitheron. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
